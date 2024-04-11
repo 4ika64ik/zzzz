93568f00dfc569a5d3306f1cb4794028ff15a26a
John Doe <k3655692@gmail.com>
2024-04-11 13:33:51 +0300

    sdfsdf

--- src/components/form/TheForm.jsx
+++ src/components/form/TheForm.jsx
@@ -76,16 +76,7 @@ export default function TheForm({ label, iconURL, backgroundColor, textColor, bo
                                 <p className="text-gray-400  text-center sm:w-full lg:sm:w-56 pb-4">Проработка
                                     индивидуальных условий под каждого клиента</p>
 
-                                <div className="my-5 bg-[#E5F0FF] py-2 px-1 rounded-s">
-                                    <label className="block text-md font-bold text-gray-600">
-                                        Life Change
-                                    </label>
-                                    <div className="flex space-y-2 justify-between items-center">
-                                        <p className="text-gray-500">
-                                            Мы с вами с 2010 года
-                                        </p>
-                                    </div>
-                                </div>
+                               
                                 <form onSubmit={handleSubmit}>
                                     <div>
                                         <label>
